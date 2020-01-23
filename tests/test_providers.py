4c4a91591ae0fadc32eed4387a5ef22e57abe606
Matthew Evans <me388@cam.ac.uk>
2020-01-23 10:52:53 +0000

    Switched to validating responses, pending updates to optimade-python-tools

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -3,7 +3,7 @@
 import pathlib
 import json
 import unittest
-from optimade.models import ProviderResource, BaseInfoResource
+from optimade.models import InfoResponse, LinksResponse
 
 TOP_DIR = pathlib.Path(__file__).parent.parent
 VERSIONS = [v.parts[-1] for v in TOP_DIR.iterdir() if v.is_dir() and v.parts[-1].startswith('v')]
@@ -17,7 +17,7 @@ class ProvidersValidator(unittest.TestCase):
             path = pathlib.Path(f"/{TOP_DIR}/{version}/info/index.html").resolve()
             with open(path, 'r') as f:
                 json_rep = json.load(f)
-            BaseInfoResource(**json_rep["data"])
+            InfoResponse(**json_rep)
 
     def test_providers(self):
         """ Validates the providers.json as a list of `Provider`s objects. """
@@ -25,6 +25,4 @@ class ProvidersValidator(unittest.TestCase):
             path = pathlib.Path(f"{TOP_DIR}/src/{version}/providers.json").resolve()
             with open(path, 'r') as f:
                 json_rep = json.load(f)
-            print(json_rep)
-            for provider in json_rep["data"]:
-                ProviderResource(**provider)
+            LinksResponse(**json_rep)
