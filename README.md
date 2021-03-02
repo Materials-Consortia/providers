3bf82966ce039bb00c6a35744b747c3a554c029a
jpdong00 <jpdong00@gmail.com>
2021-03-02 10:44:18 +0800

    Add index-metadbs/matcloud

---
+++ README.md
@@ -0,0 +1,75 @@
+<a href="https://www.optimade.org/"><img src="https://avatars0.githubusercontent.com/u/23107754" align="left" width="10%" ></a>
+
+# OPTIMADE Providers Index Meta-Database
+
+The list of providers is published as an [OPTIMADE](https://www.optimade.org/) Index Meta-Database here: [https://providers.optimade.org/](https://providers.optimade.org/)
+
+You can obtain the list of providers in a machine-readable format following the [OPTIMADE specification for Index Meta-Databases](https://github.com/Materials-Consortia/OPTIMADE/blob/master/optimade.rst#index-meta-database) by using the following URL:
+
+- [https://providers.optimade.org/v1/links](https://providers.optimade.org/v1/links)
+
+where `v1` is currently the latest version and can be replaced with any major version name of the OPTIMADE specification.
+
+For convenience, you can also access the most recent list of providers in the format mandated by the latest version of the OPTIMADE specification directly at this URL:
+
+- [https://providers.optimade.org/providers.json](https://providers.optimade.org/providers.json)
+
+Additionally, there is a providers dashboard that includes the results of validation of listed implementations.
+This can be accessed at the following URL:
+
+- [https://optimade.org/providers-dashboard](https://optimade.org/providers-dashboard)
+
+You can contribute a new provider or amend the current information by creating a pull request to [the `providers` repository](https://github.com/Materials-Consortia/providers).
+
+## Repository organization
+
+The OPTIMADE providers repository is hosted here: [https://github.com/Materials-Consortia/providers](https://github.com/Materials-Consortia/providers)
+
+The repository is organized this way:
+
+- `/src/links/<version>/providers.json` is the current providers.json file formatted according to OPTIMADE version `<version>` and any later version that uses a format that is backward compatible with this version.
+
+- `/src/info/<version>/info.json` is the proper response to the info endpoint formatted according to OPTIMADE version `<version>` and any later version that uses a format that is backward compatible with this version.
+
+- `/src/index-metadbs/<provider_name>/<version>/info.json` and `/src/index-metadbs/<provider_name>/<version>/links.json` are static Index Meta-Databases that are hosted in this repository for those providers that only have one main sub-database (or very few sub-databases) and do not wish to maintain one on their own.
+  See more details and instructions in `/src/index-metadbs/README.md`.
+
+- `/_redirects` specifies http rewrites to map index meta-database URLs `/<version>/info` and `/<version>/links` to the corresponding files under `src/`, as well as `/providers.json`.
+  It also creates `/index-metadbs/<provider_name>/<version>/info.json` and `/index-metadbs/<provider_name>/<version>/links.json` URLs to point to the corresponding files in the `/src/index-metadbs` subfolders.
+  This is used by the deploy tool Netlify.
+
+Presently, only the `v1` version of the formats exist.
+Hence, to update the list of providers, file a pull-request against the repository to edit `/src/links/v1/providers.json`.
+
+## Requirements to be listed in this providers list
+
+It is a policy of this providers list ([providers.optimade.org](http://providers.optimade.org)) that links inside `providers.json` must be links to an [OPTIMADE Index Meta-Database](https://github.com/Materials-Consortia/OPTIMADE/blob/master/optimade.rst#index-meta-database).
+
+If you only have one or few databases in your implementation, and you do not want to host an Index Meta-Database yourself, you can host the Index Meta-Database directly in this repository.
+You can find instructions [here](./src/index-metadbs).
+
+## Fork this repository to set up an index meta-database for your own databases
+
+- Click on the Fork button in GitHub and follow the instructions.
+
+- Edit `/src/info/v1/info.json` with your information:
+
+  - Make sure to update the URL in `available_api_versions` to point at your hosting location.
+
+- Copy the file `src/index-metadbs/exmpl/v1/links.json` to `/src/info/v1/links.json` and edit this template to supply information about your own databases.
+
+- Edit `README.md` to say who you are and what databases you provide.
+
+- Remove the directory 'src/static-index-metadbs'
+
+- Configure your hosting provider to use your forked repository.
+  The repository presently contains configuration files for Netlify, which you can set up as follows:
+
+  - Deploy using the Netlify *Continuous Deployment: GitHub App* option, and give it access to your forked repository with the following settings:
+  
+    - Build commmand: `jekyll build`
+    - Publish directory: `_site/`
+
+  - You are also recommended to configure a subdomain in *Domain settings* at Netlify, or setup your own custom domain.
+
+- If you are a provider of OPTIMADE databases and you have set up the index meta-database to point at them, please post a pull-request against [https://github.com/Materials-Consortia/providers](https://github.com/Materials-Consortia/providers) to add the URL for your index meta-database to the central OPTIMADE providers list.
---
+++ tests/test_providers.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+
+import pathlib
+import json
+import ssl
+import unittest
+import urllib.request
+import urllib.parse
+
+from optimade.models import IndexInfoResponse, LinksResponse, Link
+
+TOP_DIR = pathlib.Path(__file__).parent.parent
+
+# To be disabled if/when we do not wish to allow to point to v0 endpoints
+apply_v0_workarounds = True
+
+
+def query_optimade(url):
+    """Perform a URL request to the given URL endpoint.
+
+    However, for requests to `providers.optimade.org`, uses the local files
+    rather than going to fetch it on the web.
+    This is important to allow testing of new endpoints before the PR
+    is merged in GitHub (and only after things are merged in the main branch,
+    they will appear on `providers.optimade.org`)
+
+    :param url: a string with the URL to fetch
+    :return: the raw content.
+    :raise urllib.error.HTTPError: if the page is not found. Note that this exception
+        is raised also when emulating a request from the local disk for
+        `providers.optimade.org` and the file is not found; in this case the code is 404.
+    """
+    # Set to True for now - we might want to decide that this is a problem instead
+    # and set this to False
+    ignore_SSL_errors = True
+
+    # Create the context, possibly ignoring SSL errors
+    ctx = ssl.create_default_context()
+    if ignore_SSL_errors:
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+
+    parsed_url = urllib.parse.urlparse(url)
+    if parsed_url.netloc == "providers.optimade.org":
+        # Strip initial /
+        path = (
+            parsed_url.path[1:] if parsed_url.path.startswith("/") else parsed_url.path
+        )
+        # Remove any pending slashes
+        path = path[:-1] if path.endswith("/") else path
+        # Append the .json extension
+        path += ".json"
+        # Create the abs path to the file
+        file_path = TOP_DIR / "src" / path
+        try:
+            with open(file_path) as fhandle:
+                response_content = fhandle.read()
+        except FileNotFoundError:
+            raise urllib.error.HTTPError(
+                url=url, code=404, msg="HTTP Error 404: Not Found", hdrs="", fp=""
+            )
+    else:
+        with urllib.request.urlopen(url, context=ctx) as url_response:
+            response_content = url_response.read()
+
+    return response_content
+
+
+class ProvidersValidator(unittest.TestCase):
+    def test_info(self):
+        """ Validates the index.html json fudge as a `BaseInfoResource` object. """
+        info_dir = TOP_DIR / "src" / "info"
+        versions = [
+            v.parts[-1]
+            for v in info_dir.iterdir()
+            if v.is_dir() and v.parts[-1].startswith("v")
+        ]
+        for version in versions:
+            path = pathlib.Path(f"/{TOP_DIR}/src/info/{version}/info.json").resolve()
+            with open(path, "r") as f:
+                json_rep = json.load(f)
+            IndexInfoResponse(**json_rep)
+
+    def test_providers(self):
+        """ Validates the providers.json as a list of `Provider`s objects. """
+        links_dir = TOP_DIR / "src" / "links"
+        versions = [
+            v.parts[-1]
+            for v in links_dir.iterdir()
+            if v.is_dir() and v.parts[-1].startswith("v")
+        ]
+        for version in versions:
+            path = pathlib.Path(
+                f"{TOP_DIR}/src/links/{version}/providers.json"
+            ).resolve()
+            with open(path, "r") as f:
+                json_rep = json.load(f)
+            LinksResponse(**json_rep)
+
+    def test_index_metadb(self):
+        """ Validates that all (non-null) entries in providers.json point to an index meta-db. """
+        # We collect all errors and report all of them at the end, see below
+        problems = []
+
+        links_dir = TOP_DIR / "src" / "links"
+        versions = [
+            v.parts[-1]
+            for v in links_dir.iterdir()
+            if v.is_dir() and v.parts[-1].startswith("v")
+        ]
+        for version in versions:
+            path = pathlib.Path(
+                f"{TOP_DIR}/src/links/{version}/providers.json"
+            ).resolve()
+            with open(path, "r") as f:
+                json_rep = json.load(f)
+            response = LinksResponse(**json_rep)
+            for entry in response.data:
+                entry_id = entry.id
+                if entry.attributes.dict().get("base_url", None) is not None:
+                    # the provider has a non-null base_url
+                    print(f'[INFO] Checking provider "{entry_id}" ({version})')
+
+                    # I check the /info endpoint
+                    entry_base_url = (
+                        entry.attributes.base_url.href
+                        if isinstance(entry.attributes.base_url, Link)
+                        else entry.attributes.base_url
+                    )
+                    info_endpoint = f"{entry_base_url}/{version}/info"
+                    tested_info_endpoints = [info_endpoint]
+                    try:
+                        try:
+                            response_content = query_optimade(info_endpoint)
+                        except urllib.error.HTTPError as exc:
+                            if (
+                                apply_v0_workarounds
+                                and version == "v1"
+                                and exc.code == 404
+                            ):
+                                try:
+                                    # Temporary workaround for optimade-python-tools while v1 is released
+                                    info_endpoint = f"{entry_base_url}/v0.10/info"
+                                    tested_info_endpoints.append(info_endpoint)
+                                    response_content = query_optimade(info_endpoint)
+                                except urllib.error.HTTPError as exc:
+                                    # Temporary workaround for nomad that uses v0 as a prefix
+                                    info_endpoint = f"{entry_base_url}/v0/info"
+                                    tested_info_endpoints.append(info_endpoint)
+                                    response_content = query_optimade(info_endpoint)
+                            else:
+                                raise
+                    except urllib.error.HTTPError as exc:
+                        fallback_string = (
+                            ""
+                            if len(tested_info_endpoints) == 1
+                            else f" (I tried all these URLs: {tested_info_endpoints})"
+                        )
+                        problems.append(
+                            f'Provider "{entry_id}" {info_endpoint} endpoint is not reachable{fallback_string}. Error: {str(exc)}'
+                        )
+                        continue
+
+                    try:
+                        info_response = IndexInfoResponse(
+                            **json.loads(response_content)
+                        )
+                    except Exception as exc:
+                        problems.append(
+                            f'Provider "{entry_id}": {info_endpoint} endpoint has problems during validation.\nError message:\n{str(exc)}'
+                        )
+                        continue
+
+                    # If unspecified, it should be assumed as False, according to the OPTIMADE specs
+                    is_index = info_response.data.attributes.dict().get(
+                        "is_index", False
+                    )
+                    if not is_index:
+                        print(f"  > PROBLEM DETECTED with provider '{entry_id}'.")
+                        print(response_content)
+                        problems.append(
+                            f'Provider "{entry_id}" is NOT providing an index meta-database at {info_endpoint}'
+                        )
+                        continue
+
+                    print(
+                        f'[INFO] Provider "{entry_id}" ({version}) validated correctly ({info_endpoint})'
+                    )
+
+        # I am collecting all problems and printing at the end because in this way we get a full overview
+        if problems:
+            err_msg = "PROBLEMS DETECTED!\n\n" + "\n\n".join(problems)
+            # Prepend with [ERROR] so that it gets colored in the GitHub output
+            err_msg = "\n".join(f"[ERROR] {line}" for line in err_msg.splitlines())
+            print(err_msg)
+            raise AssertionError(err_msg)
